ee73ecf90af7cabd04063038d21d1c8bf491805d
swaroop kumar <swaroop@anirasolutions.net>
2012-05-30 20:05:35 +0530

    post created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Testapp::Application.routes.draw do
-  get "home/index"
+  resources :posts
+
+  get "posts/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +52,7 @@ Testapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'posts#index'
 
   # See how all your routes lay out with "rake routes"
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120530142948) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
