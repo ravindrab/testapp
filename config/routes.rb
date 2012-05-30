9abf8eca68c664ea637c15bab97981b712f3130e
swaroop kumar <swaroop@anirasolutions.net>
2012-05-30 20:11:55 +0530

    root file changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Testapp::Application.routes.draw do
   resources :posts
 
-  get "posts/index"
+  get "home/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +52,7 @@ Testapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'posts#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
