cbc82a59d98a38c58d4e17eac8831774510b8c9a
swaroop kumar <swaroop@anirasolutions.net>
2012-06-01 17:43:54 +0530

    no changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
-validates_format_of :email, :presence => true, :uniqueness => true
+
+validates_format_of :name, :presence => true, :uniqueness => true
    
-=======
-validates_format_of :name, :with => /^[a-zA-Z]*$/i,:message =>
+
+validates_format_of :email, :with => /^[a-zA-Z]*$/i,:message =>
 "can only contain letters ."
->>>>>>> f8bf132e6eddbc5aa488cc51f7f1ee1c3118820a
+
 end
