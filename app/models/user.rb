579712aa7f0104fd4771122e7c10132fddb3cb3b
swaroop kumar <swaroop@anirasolutions.net>
2012-06-01 18:55:32 +0530

    s4

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+
+validates_format_of :name, :presence => true, :uniqueness => true
+   
+
+validates_format_of :email, :with => /^[a-zA-Z]*$/i,:message =>
+"can only contain letters ."
+
+
+end
