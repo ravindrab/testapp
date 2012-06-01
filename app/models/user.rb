a9a59cc8e2a9ee642ecd826eefb3308c16ad231d
Ravindra <ravindra@anirasolutions.net>
2012-06-01 18:15:13 +0530

    validations r used

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
 validates_format_of :name, :with => /^[a-zA-Z]*$/i,:message =>
 "can only contain letters ."
+validates_confirmation_of :user_name, :password
+      validates_confirmation_of :email_address, :message => "should match confirmation"
 end
