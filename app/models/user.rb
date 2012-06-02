dd8e2e1091462749bd3b12ae745ff14e453ca211
Ravindra <ravindra@anirasolutions.net>
2012-06-02 11:27:36 +0530

    contactus chages r done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     validates_format_of :name, :with => /^[a-zA-Z]*$/i,:message =>
      "can only contain letters ."
-validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
    
 end
